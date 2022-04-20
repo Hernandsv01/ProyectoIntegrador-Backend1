570b253b26f5c0b5b428dde29bc9f1f286c07e4b
Hernandsv01 <hernanxdsv@gmail.com>
2022-04-20 17:42:00 -0300

    Added full paciente and domicilio structure

---
+++ src/main/java/com/dh/integrador/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.dh.integrador.repository;
+
+import com.dh.integrador.entities.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
